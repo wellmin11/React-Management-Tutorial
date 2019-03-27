aa0b1bbe83f8cd76a96cebf8f5e3d80f7589fdbd
Hyunmin Lee <wellmin11@naver.com>
2019-03-27 18:04:13 +0900

    Apply Material UI

--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,44 +1,25 @@
 import React from 'react';
+import TableRow from '@material-ui/core/TableRow'
+import TableCell from '@material-ui/core/TableCell'
 
-// React.Component 는 일종의 라이브러리자 컴포넌트라고 할 수 있다.
 class Customer extends React.Component{
-    // 항상 수행되는 내용으로써 실제 웹에 그려질 내용들을 담고 있는 곳
+    
     render(){
-        // 실제로 보여질 내용들은 리턴 형태로 되어있어야 에러가 안난다.
+        
         return(
-            // 하나이상의 정보들을 보여줄때는  div를 꼭 사용해야한다 
-            <div>
-                <CustomerProfile id = {this.props.id} image = {this.props.image} name= {this.props.name} />
-                <CustomerInfo birthday={this.props.birthday} gender={this.props.gender} job={this.props.job} />
-            </div>
+            <TableRow>
+                <TableCell>{this.props.id}</TableCell>
+                <TableCell><img src = {this.props.image} alt = "profile"/></TableCell>
+                <TableCell>{this.props.name}</TableCell>
+                <TableCell>{this.props.birthday}</TableCell>
+                <TableCell>{this.props.gender}</TableCell>
+                <TableCell>{this.props.job}</TableCell>
+            </TableRow>
         )
         
     }
 }
 
-class CustomerProfile extends React.Component{
-    render(){
-        return(
-            <div>
-                <img src = {this.props.image} alt = "profile" />
-                <h2>{this.props.name}({this.props.id})</h2>
-            </div>
-        )
-    }
-}
 
-
-class CustomerInfo extends React.Component{
-    render(){
-        return(
-            <div>
-                <p>{this.props.birthday}</p>
-                <p>{this.props.gender}</p>
-                <p>{this.props.job}</p>
-            </div>
-        )
-    }
-
-}
 
 export default Customer;
